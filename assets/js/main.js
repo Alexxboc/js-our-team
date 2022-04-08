7a95415967d8bdb40a23a7ac662a6c381e9ee732
Alexxboc <boccardi.alessandro@gmail.com>
2022-04-08 15:55:17 +0200

    add form function

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,23 +53,80 @@ const rowElement = document.querySelector('.row')
 
 // Creo un ciclo for
 for (let i = 0; i < team.length; i++) {
-    const info = team[i];
+    const member = team[i];
     // Creo l'elemento da appendere alla dom
-    let colHtmlElement = `
-    <div class="col">
-        <img src="./assets/img/${info['image']}" alt="">
-        <div class="name">${info['name']}</div>
-        <div class="role">${info['role']}</div>
-    </div>`
+    // let colHtmlElement = 
+    // `
+    // <div class="col">
+    //     <img src="./assets/img/${info['image']}" alt="">
+    //     <div class="name">${info['name']}</div>
+    //     <div class="role">${info['role']}</div>
+    // </div>
+    // `
     // appendo l'elemento alla DOM
-    rowElement.insertAdjacentHTML('beforeend', colHtmlElement);
+    const memberMarkup = generateMemberMarkup(member)
+    rowElement.insertAdjacentHTML('beforeend', memberMarkup);
 
 
     // console.log(info['name']);
     // console.log(info['role']);
     // console.log(info['image']);
     // console.log('_____________');
-
-  
 }
+
+/**
+ * Generate dinamic markup for the members card
+ * @param {object} member A member object 
+ */
+
+function generateMemberMarkup(member_object) {
+  
+  return  `
+  <div class="col">
+      <img src="./assets/img/${member_object['image']}" alt="">
+      <div class="name">${member_object['name']}</div>
+      <div class="role">${member_object['role']}</div>
+  </div>
+  `
+}
+
+/* 
+Compilare il form e prendere i valori inseriti e creare un nuovo oggetto da stampare a schermo con gli altri
+*/
+
+// Selezionare il form
+const formElement = document.querySelector(`form`)
+// Aggiungere eventListener
+formElement.addEventListener('submit', function(event){
+  // Prevenire refresh della pagina
+  event.preventDefault()
+  // Estrapolare valore dei campi compilati
+  const nameValue = event.target.name.value
+  const roleValue = event.target.role.value
+  const imageValue = event.target.image.value
+  console.log(nameValue, roleValue, imageValue);
+  // Usare i valori presio sopra e creare un nuovo oggetto
+  const newMember = {
+    name: nameValue,
+    role: roleValue,
+    image: imageValue
+  }
+  // Pushare il nuovo oggetto nell'array
+  team.push(newMember)
+  console.log(team);
+  // rigenerare la dom con le card
+  const memberMarkup = generateMemberMarkup(newMember)
+  rowElement.insertAdjacentHTML('beforeend', memberMarkup);
+  // ripulire gli input prima dell'invio si un nuovo form
+  document.getElementById('name').value = ''
+  document.getElementById('role').value = ''
+  document.getElementById('image').value = ''
+
+  // Impedire di inviare form senza compilare campi
+  // Aggiungere required agli input
+
+})
+
+
+
 
